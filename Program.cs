8314f8af1fb3d9538b55f5a6dd3f9cc3a429cbaa
Maikle Matveev <Maikle@Maikle-iMac.local>
2022-05-11 21:38:38 +0300

    Всё работает! Заканчиваем работу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿string[] arraystr = new string[] { "Коля", "Оля", "Пашка", "Руслан", "Олег", "Денис", "Миша" };
 Random rand = new Random();
+int i = rand.Next(0,6);
+int x = rand.Next(0,6);
+int y = rand.Next(0,6);
+
+string[] arraystr3 = new string[] {arraystr[i], arraystr[x], arraystr[y]};
+
+for (int z = 0; z < arraystr3.Length; z++)
+{
+    Console.WriteLine(arraystr3[z]);
+}
